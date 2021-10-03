680d26715be64be83d926c8c7c71da97458d3720
Eric Weitz <eweitz@broadinstitute.org>
2021-10-03 11:57:11 -0400

    Refactor citations code to class, for testability

--- gene_hints/citations/citations.py
+++ gene_hints/citations/citations.py
@@ -21,9 +21,9 @@ from lib import read_organisms
 from enrich_citations import enrich_citations
 from pmids_by_date import pmids_by_date
 
-cites_dir = "./pubmed_citations/"
-data_dir = cites_dir + "data/"
-tmp_dir = data_dir + "tmp/"
+# cites_dir = "./pubmed_citations/"
+# data_dir = cites_dir + "data/"
+# tmp_dir = data_dir + "tmp/"
 
 def format_date(days_before=None):
     """Get date strings in YYYY/MM/DD format, as expected by NCBI E-utils
@@ -49,34 +49,6 @@ def download_gzip(url, output_path):
     with open(output_path, "w") as f:
         f.write(content)
 
-def split_ncbi_file_by_org(input_path, output_filename, organisms):
-    """Split a multi-organism file from NCBI into organism-specific files
-
-    Input file must be a TSV file with taxid as first column
-    """
-    org_names_by_taxid = {}
-    for org in organisms:
-        taxid = org["taxid"]
-        name = org["scientific_name"]
-        org_names_by_taxid[taxid] = name
-
-    with open(input_path, "r") as f:
-        lines_by_org = {}
-        for line in f:
-            line = line.strip()
-            taxid = line.split("\t")[0]
-            if taxid in org_names_by_taxid:
-                org = org_names_by_taxid[taxid] # scientific name
-                if org in lines_by_org:
-                    lines_by_org[org].append(line)
-                else:
-                    lines_by_org[org] = [line]
-
-        for org in lines_by_org:
-            lines = lines_by_org[org]
-            with open(data_dir + org + "/" + output_filename, "w") as f:
-                f.write("\n".join(lines))
-
 def merge_daily_pmids(output_path, daily_pmid_dir):
     """Aggregate per-day files into one file, to ease downstream processing
     """
@@ -94,109 +66,148 @@ def merge_daily_pmids(output_path, daily_pmid_dir):
         lines = "\n".join(pmids)
         f.write(lines)
 
-def fetch_all_publications_over_time(path, prev_path, num_days):
-    """Download IDs for articles published in the last `num_days`
-    """
-    start_date = format_date(num_days) # E.g. 60 days ago
-    end_date = format_date() # Today
-    prev_start_date = format_date(num_days * 2) # E.g. 120 days ago
-    prev_end_date = start_date # E.g. 60 days ago
+class Citations():
 
-    output_dir = tmp_dir + "timeframe"
-    prev_output_dir= tmp_dir + "prev_timeframe"
+    def __init__(
+        self,
+        cites_dir="./pubmed_citations/"
+    ):
+        self.cites_dir = cites_dir
+        self.data_dir = cites_dir + "data/"
+        self.tmp_dir = self.data_dir + "tmp/"
 
-    pmids_by_date(start_date, end_date, output_dir)
-    pmids_by_date(prev_start_date, prev_end_date, prev_output_dir)
+    def split_ncbi_file_by_org(self, input_path, output_filename, organisms):
+        """Split a multi-organism file from NCBI into organism-specific files
 
-    print("Combine daily publication counts")
-    merge_daily_pmids(path, output_dir)
-    merge_daily_pmids(prev_path, prev_output_dir)
+        Input file must be a TSV file with taxid as first column
+        """
+        org_names_by_taxid = {}
+        for org in organisms:
+            taxid = org["taxid"]
+            name = org["scientific_name"]
+            org_names_by_taxid[taxid] = name
 
-def fetch_all_publications_per_organism(organisms):
-    """Get IDs for articles published about our organisms of interest
-    """
-    for org in organisms:
-        dir = data_dir + org["scientific_name"]
-        if not os.path.exists(dir):
-            os.makedirs(dir)
+        with open(input_path, "r") as f:
+            lines_by_org = {}
+            for line in f:
+                line = line.strip()
+                taxid = line.split("\t")[0]
+                if taxid in org_names_by_taxid:
+                    org = org_names_by_taxid[taxid] # scientific name
+                    if org in lines_by_org:
+                        lines_by_org[org].append(line)
+                    else:
+                        lines_by_org[org] = [line]
 
-    print("Download gene2pubmed")
-    url = "https://ftp.ncbi.nlm.nih.gov/gene/DATA/gene2pubmed.gz"
-    output_filename = "gene2pubmed"
-    output_path = data_dir + output_filename
-    download_gzip(url, output_path)
+            for org in lines_by_org:
+                lines = lines_by_org[org]
+                output_path = self.data_dir + org + "/" + output_filename
+                with open(output_path, "w") as f:
+                    f.write("\n".join(lines))
 
-    print("Split gene2pubmed by organism")
-    split_ncbi_file_by_org(output_path, output_filename, organisms)
+    def fetch_all_publications_over_time(self, path, prev_path, num_days):
+        """Download IDs for articles published in the last `num_days`
+        """
+        start_date = format_date(num_days) # E.g. 60 days ago
+        end_date = format_date() # Today
+        prev_start_date = format_date(num_days * 2) # E.g. 120 days ago
+        prev_end_date = start_date # E.g. 60 days ago
 
-def fetch_genome_annotations(organisms):
-    """Download GTF files from UCSC; these contain genomic coordinates, etc.
-    """
-    print("Download UCSC genome annotations for each organism")
-    for org in organisms:
-        org_name = org["scientific_name"]
-        asm_ucsc_name = org["genome_assembly_ucsc_name"]
+        output_dir = self.tmp_dir + "timeframe"
+        prev_output_dir= self.tmp_dir + "prev_timeframe"
 
-        # Construct parameters for fetching UCSC genome annotation files,
-        # e.g. https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/genes/hg38.refGene.gtf.gz
-        base_url = "https://hgdownload.soe.ucsc.edu/goldenPath/"
-        leaf = "refGene.gtf"
-        if org_name in ["homo-sapiens", "rattus-norvegicus", "felis-catus"]:
-            leaf = asm_ucsc_name + "." + leaf
-        url = base_url + asm_ucsc_name + "/bigZips/genes/" + leaf + ".gz"
-        output_path = data_dir + org_name + "/" + leaf
+        pmids_by_date(start_date, end_date, output_dir)
+        pmids_by_date(prev_start_date, prev_end_date, prev_output_dir)
 
+        print("Combine daily publication counts")
+        merge_daily_pmids(path, output_dir)
+        merge_daily_pmids(prev_path, prev_output_dir)
+
+    def fetch_all_publications_per_organism(self, organisms):
+        """Get IDs for articles published about our organisms of interest
+        """
+        for org in organisms:
+            dir = self.data_dir + org["scientific_name"]
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+
+        print("Download gene2pubmed")
+        url = "https://ftp.ncbi.nlm.nih.gov/gene/DATA/gene2pubmed.gz"
+        output_filename = "gene2pubmed"
+        output_path = self.data_dir + output_filename
         download_gzip(url, output_path)
 
-def fetch_gene_info(organisms):
-    print("Download gene_info")
-    url = "https://ftp.ncbi.nlm.nih.gov/gene/DATA/gene_info.gz"
-    output_filename = "gene_info"
-    output_path = data_dir + output_filename
-    download_gzip(url, output_path)
+        print("Split gene2pubmed by organism")
+        self.split_ncbi_file_by_org(output_path, output_filename, organisms)
 
-    print("Split gene_info by organism")
-    split_ncbi_file_by_org(output_path, output_filename, organisms)
+    def fetch_genome_annotations(self, organisms):
+        """Download GTF files from UCSC; these contain genomic coordinates, etc.
+        """
+        print("Download UCSC genome annotations for each organism")
+        for org in organisms:
+            org_name = org["scientific_name"]
+            asm_ucsc_name = org["genome_assembly_ucsc_name"]
 
-def download_data(pmid_dates_path, prev_pmid_dates_path, num_days):
-    """Download citation and genomic data, preparing it for enrichment
-    """
+            # Construct parameters for fetching UCSC genome annotation files,
+            # e.g. https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/genes/hg38.refGene.gtf.gz
+            base_url = "https://hgdownload.soe.ucsc.edu/goldenPath/"
+            leaf = "refGene.gtf"
+            if org_name in ["homo-sapiens", "rattus-norvegicus", "felis-catus"]:
+                leaf = asm_ucsc_name + "." + leaf
+            url = base_url + asm_ucsc_name + "/bigZips/genes/" + leaf + ".gz"
+            output_path = self.data_dir + org_name + "/" + leaf
 
-    # Make tmp_dir, and thereby also the other dirs
-    if not os.path.exists(tmp_dir):
-        os.makedirs(tmp_dir)
+            download_gzip(url, output_path)
 
-    # Download IDs for articles published in the last `num_days`
-    fetch_all_publications_over_time(
-        pmid_dates_path, prev_pmid_dates_path, num_days
-    )
+    def fetch_gene_info(self, organisms):
+        print("Download gene_info")
+        url = "https://ftp.ncbi.nlm.nih.gov/gene/DATA/gene_info.gz"
+        output_filename = "gene_info"
+        output_path = self.data_dir + output_filename
+        download_gzip(url, output_path)
 
-    organisms = read_organisms()
+        print("Split gene_info by organism")
+        self.split_ncbi_file_by_org(output_path, output_filename, organisms)
 
-    # Download IDs for articles published about our organisms of interest.
-    # We filter and join the "publications over time" and "publications per
-    # organism" lists in `enrich_citations`.
-    fetch_all_publications_per_organism(organisms)
+    def download_data(self, pmid_dates_path, prev_pmid_dates_path, num_days):
+        """Download citation and genomic data, preparing it for enrichment
+        """
 
-    # Download GTF files from UCSC; these contain genomic coordinates, etc.
-    fetch_genome_annotations(organisms)
+        # Make tmp_dir, and thereby also the other dirs
+        if not os.path.exists(self.tmp_dir):
+            os.makedirs(self.tmp_dir)
 
-    # Download more genomic information
-    # TODO: Is data parsed from gene_info available in UCSC GTF files?
-    fetch_gene_info(organisms)
+        # Download IDs for articles published in the last `num_days`
+        self.fetch_all_publications_over_time(
+            pmid_dates_path, prev_pmid_dates_path, num_days
+        )
 
-def gene_citations(num_days):
-    """Output TSV of gene citation counts and related metrics over `num_days`
-    """
+        organisms = read_organisms()
 
-    pmid_dates_path = data_dir + "pmid_dates.tsv"
-    prev_pmid_dates_path = data_dir + "prev_pmid_dates.tsv"
+        # Download IDs for articles published about our organisms of interest.
+        # We filter and join the "publications over time" and "publications per
+        # organism" lists in `enrich_citations`.
+        self.fetch_all_publications_per_organism(organisms)
 
-    # Download citation and genomic data
-    download_data(pmid_dates_path, prev_pmid_dates_path, num_days)
+        # Download GTF files from UCSC; these contain genomic coordinates, etc.
+        self.fetch_genome_annotations(organisms)
 
-    # Combine that downloaded data, compute statistics, and write to TSV
-    enrich_citations(pmid_dates_path, prev_pmid_dates_path, num_days)
+        # Download more genomic information
+        # TODO: Is data parsed from gene_info available in UCSC GTF files?
+        self.fetch_gene_info(organisms)
+
+    def gene_citations(self, num_days):
+        """Output TSV of gene citation counts and related metrics over `num_days`
+        """
+
+        pmid_dates_path = self.data_dir + "pmid_dates.tsv"
+        prev_pmid_dates_path = self.data_dir + "prev_pmid_dates.tsv"
+
+        # Download citation and genomic data
+        self.download_data(pmid_dates_path, prev_pmid_dates_path, num_days)
+
+        # Combine that downloaded data, compute statistics, and write to TSV
+        enrich_citations(pmid_dates_path, prev_pmid_dates_path, num_days)
 
 # Command-line handler
 if __name__ == "__main__":
@@ -212,4 +223,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
     num_days = args.num_days
 
-    gene_citations(num_days)
+    citations = Citations()
+
+    citations.gene_citations(num_days)
